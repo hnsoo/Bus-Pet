6b5a521bb8ccf0cdf7c0b546613f893855f97940
hnsoo <khsoo2439@gmail.com>
2022-03-21 21:11:09 +0900

    FEAT: 버스 검색 기능 UI 개발

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router(); 
 
 router.get('/', (req, res) => { 
-    res.send("gdfidj") 
+    res.sendFile(__dirname + '../src/html/index.html') 
 }); 
 
 module.exports = router;
