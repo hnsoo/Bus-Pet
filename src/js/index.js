19f4b2c9111fa69b017a79751e15e086345baf38
hnsoo <khsoo2439@gmail.com>
2022-03-21 22:30:51 +0900

    Refactor: attr 정리

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,15 @@ $("input[type=radio][name=type]").on('click', () => {
 
     console.log(valueCheck);
     if (valueCheck == 'bus_route') {
-        $("input[type=search]").attr('placeholder', '버스 번호 입력');
-        $("input[type=search]").attr('onfocus', 'this.placeholder=""');
-        $("input[type=search]").attr('onblur', "this.placeholder='버스 번호 입력'");
+        $("input[type=search]").attr({
+            placeholder: '버스 번호 입력',
+            onfocus: 'this.placeholder=""',
+            onblur: "this.placeholder='버스 번호 입력'"});
     }
     if (valueCheck == 'bus_stop') {
-        $("input[type=search]").attr('placeholder', '정류장 이름 입력');
-        $("input[type=search]").attr('onfocus', 'this.placeholder=""');
-        $("input[type=search]").attr('onblur', "this.placeholder='정류장 이름 입력'");
+        $("input[type=search]").attr({
+            placeholder: '정류장 이름 입력',
+            onfocus: 'this.placeholder=""',
+            onblur: "this.placeholder='정류장 이름 입력'"});
     }
 });
