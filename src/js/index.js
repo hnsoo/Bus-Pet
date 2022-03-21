70b79f7d84a1e4b1836f3856f676ff5af8dcb2c5
hnsoo <khsoo2439@gmail.com>
2022-03-21 21:49:09 +0900

    FEAT: 메인 페이지 UI
    노선, 정류장 버튼을 누르면 입력창 내용이 변경되게함

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+$("input[type=radio][name=type]").on('click', () => {
+    var valueCheck = $("input[type=radio][name=type]:checked").val();
+
+    console.log(valueCheck);
+    if (valueCheck === 'bus_route') {
+        $("input[type=search]").val('버스 번호 입력');
+    }
+    if (valueCheck === 'bus_stop') {
+        $("input[type=search]").val('정류장 이름 입력');
+    }
+});
