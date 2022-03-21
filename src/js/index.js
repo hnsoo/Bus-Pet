cc10d5a2451ada48968f29fc413b9ab5106cd61a
hnsoo <khsoo2439@gmail.com>
2022-03-21 22:27:49 +0900

    FEAT: 메인 UI 개발
    입력창에 예비 내용 추가

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,14 @@ $("input[type=radio][name=type]").on('click', () => {
     var valueCheck = $("input[type=radio][name=type]:checked").val();
 
     console.log(valueCheck);
-    if (valueCheck === 'bus_route') {
-        $("input[type=search]").val('버스 번호 입력');
+    if (valueCheck == 'bus_route') {
+        $("input[type=search]").attr('placeholder', '버스 번호 입력');
+        $("input[type=search]").attr('onfocus', 'this.placeholder=""');
+        $("input[type=search]").attr('onblur', "this.placeholder='버스 번호 입력'");
     }
-    if (valueCheck === 'bus_stop') {
-        $("input[type=search]").val('정류장 이름 입력');
+    if (valueCheck == 'bus_stop') {
+        $("input[type=search]").attr('placeholder', '정류장 이름 입력');
+        $("input[type=search]").attr('onfocus', 'this.placeholder=""');
+        $("input[type=search]").attr('onblur', "this.placeholder='정류장 이름 입력'");
     }
 });
